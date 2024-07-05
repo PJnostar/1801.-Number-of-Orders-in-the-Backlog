246890801b49ef187ca4028e154fc37b922c7904
PJ <piotr.jurkowskii@gmail.com>
2024-07-05 22:21:54 +0200

    działa dla prostych funkcji szukania poprawnego indeksu Dla wielkich wartości orders mieli się godzinami Zacząłem pisac bin search dla funkcji szukania poprawnego indeksu

--- main.py
+++ main.py
@@ -3,4 +3,10 @@ import queue_class
 orders = [[10,5,0],[15,2,1],[25,1,1],[30,4,0]]
 
 test1 = queue_class.Solution()
-test1.getNumberOfBacklogOrders(orders)
+res = test1.getNumberOfBacklogOrders(orders)
+print(res)
+
+orders2 = [[7,1000000000,1],[15,3,0],[5,999999995,0],[5,1,1]]
+test2 = queue_class.Solution()
+res2 = test2.getNumberOfBacklogOrders(orders2)
+print(res2)
--- queue_class.py
+++ queue_class.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/number-of-orders-in-the-backlog/description/
 
+import bisect
+
 class Solution(object):
     def getNumberOfBacklogOrders(self, orders):
         """
@@ -37,6 +39,13 @@ class Solution(object):
                     # queue_inc = queue_inc[:i+1] + orders + queue_inc[i+1:]
                     index_to_add = i
                     break
+            return index_to_add
+        
+        # funkcja znajdujaca indeks, w ktorym miejscu trzeba wstawic zamowienia, dla rosnących zamowien 
+        #BISECTEM  
+        def find_where_to_add_order_in_increasing_order_bisect(queue_inc, item_price):
+            n = len(queue_inc)
+            index_to_add = bisect.bisect_left(queue_inc, item_price)
             return index_to_add
         
         # funkcja znajdujaca indeks, w ktorym miejscu trzeba wstawic zamowienia, dla malejacych zamowien
@@ -78,31 +87,45 @@ class Solution(object):
             return queue_dec
         
         #function to buy items from backlog_sell
-        def buy_items_from_backlog_sell(backlog_sell, orders):
-            for order in orders:
-                if (order[0] <= backlog_sell[0]):       #order is executed == element from backlog_sell is deleted
-                    backlog_sell.pop(0)
-                else:
+        #order contains a single order[price, amount, type], not the entire list of them
+        def buy_items_from_backlog_sell(backlog_sell, backlog_buy, order):
+            for i in range(0, order[1]):
+                if (backlog_sell == []):
                     backlog_buy = queue_dec_add_item(backlog_buy, order[0], 1)
-                return sth sth
+                else:
+                    if (order[0] >= backlog_sell[0]):       #order is executed == element from backlog_sell is deleted
+                        backlog_sell.pop(0)
+                    else:
+                        backlog_buy = queue_dec_add_item(backlog_buy, order[0], 1)
+            return [backlog_buy, backlog_sell]
 
         #function to sell items from backlog_buy
         # def sell_items_from_backlog_buy(backlog_buy, order):
-            
+        def buy_items_from_backlog_buy(backlog_sell, backlog_buy, order):
+            for i in range(0, order[1]):
+                if (backlog_buy == []):
+                    backlog_sell = queue_inc_add_item(backlog_sell, order[0], 1)
+                else:
+                    if (order[0] <= backlog_buy[0]):       #order is executed == element from backlog_sell is deleted
+                        backlog_buy.pop(0)
+                    else:
+                        backlog_sell = queue_inc_add_item(backlog_sell, order[0], 1)
+            return [backlog_buy, backlog_sell]
+
+
         #wlasciwa czesc kodu wykonujaca zadanie
         backlog_buy = []
         backlog_sell = []
         for order in orders:
             print(order)
             if (order[2] == 0):
-                buy_items_from_backlog_sell(backlog_sell, order)
-                backlog_buy = queue_inc_add_item(backlog_buy, order[0], order[1])
+                [backlog_buy, backlog_sell] = buy_items_from_backlog_sell(backlog_sell, backlog_buy, order)
             elif (order[2] == 1):
-                # sell_items_from_backlog_buy()
-                backlog_sell = queue_dec_add_item(backlog_sell, order[0], order[1])
+                [backlog_buy, backlog_sell] = buy_items_from_backlog_buy(backlog_sell, backlog_buy, order)
         print("backlog_buy: ", backlog_buy)
         print("backlog_sell: ", backlog_sell)
 
+        return (len(backlog_sell) + len(backlog_buy))%(pow(10,9)+7)
 """ 
         queue_inc_test = [1,2,3,4,5]
         item = [7,3,1]
